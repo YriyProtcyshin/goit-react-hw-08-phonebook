336bc104e44ca3ff067c1cfbaabc4853703eb2c5
YriyProtcyshin <yriy.protcyshin@gmail.com>
2023-01-24 22:57:49 +0200

    commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import { Navigation } from './Navigation';
+import { UserMenu } from './UserMenu';
+
+export const Header = () => {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="sm">
+        <Toolbar disableGutters>
+          <Navigation />
+          <UserMenu />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
---
+++ src/components/Header/UserMenu.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const settings = ['Logout', 'Settings'];
+const avatar_id = 2222;
+
+export const UserMenu = () => {
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  const handleOpenUserMenu = event => {
+    setAnchorElUser(event.currentTarget);
+  };
+  return (
+    <>
+      <Box sx={{ flexGrow: 0 }}>
+        <Tooltip title="User menu">
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <Avatar
+              alt="Remy Sharp"
+              src={`https://i.pravatar.cc/150?u=${avatar_id}`}
+            />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          sx={{ mt: '45px' }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {settings.map(setting => (
+            <MenuItem key={setting} onClick={handleCloseUserMenu}>
+              <Typography textAlign="center">{setting}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
+    </>
+  );
+};
