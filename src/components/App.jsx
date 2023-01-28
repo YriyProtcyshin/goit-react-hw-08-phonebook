3b7501f8c9d94d58e37a003ec62ad058bdf2c9d1
YriyProtcyshin <yriy.protcyshin@gmail.com>
2023-01-28 10:17:28 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout';
 import { LoginPage } from 'pages/LoginPage';
 import { SignUp } from 'pages/SignUp';
+import { AppRouter } from './AppRoute';
 
 export const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUp />} />
-    </Routes>
-  );
+  return <AppRouter />;
 };
